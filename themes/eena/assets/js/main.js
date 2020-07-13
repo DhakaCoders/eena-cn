b79f1b4b2131b9edd43b694a341892ee3f1f6397
Rannojit <cserannojitsarkar22@gmail.com>
2020-07-13 21:12:13 +0600

    .

--- themes/eena/assets/js/main.js
+++ themes/eena/assets/js/main.js
@@ -209,6 +209,47 @@ if( $('.qty').length ){
 
 
 
+if( $('.hmNewsSecSlider').length ){
+    $('.hmNewsSecSlider').slick({
+      dots: false,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: $('.hmNewsSecSliderPrevNext .fl-prev'),
+      nextArrow: $('.hmNewsSecSliderPrevNext .fl-next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
 
 
 /*End of Rannojit*/

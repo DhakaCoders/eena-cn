423f67574ebd7433b0710fe9b7e406743853b29c
Milon9787 <56488424+Milon9787@users.noreply.github.com>
2020-07-16 14:55:43 +0600

    footer pest

--- themes/eena/assets/js/main.js
+++ themes/eena/assets/js/main.js
@@ -175,6 +175,49 @@ if( $('.bdcmbSlider').length ){
 
 /* End of Milon*/
 
+if( $('.FanShopPostSlider').length ){
+    $('.FanShopPostSlider').slick({
+      dots: false,
+      infinite: false,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      prevArrow: $('.FanShopPostSliderPrevNext .fl-prev'),
+      nextArrow: $('.FanShopPostSliderPrevNext .fl-next'),
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 991,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 575,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+}
+
 
 // footer slide menu
 

337f2c129aee3be15cab4714de319b0d569c4b65
azizulsoftsw <azizul.softsw@gmail.com>
2020-07-28 01:31:03 +0600

    az

--- themes/eena/woocommerce/single-product/add-to-cart/simple.php
+++ themes/eena/woocommerce/single-product/add-to-cart/simple.php
@@ -32,22 +32,49 @@ if ( $product->is_in_stock() ) : ?>
 	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
 		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-		<?php
-		do_action( 'woocommerce_before_add_to_cart_quantity' );
+	<?php
+	do_action( 'woocommerce_before_add_to_cart_quantity' );
+	?>
+	<div class="custom-quantity">
+		<span class="d-an">Aantal</span>
+        <div class="pro-counter clearfix"> 
+			<div class="qty">
+		    <span class="minus"> <i class="fa fa-minus"></i> </span>
+		    <?php
+			woocommerce_quantity_input(
+				array(
+					'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
+					'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
+					'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+				)
+			);
+			?>
+			<span class="plus"><i class="fa fa-plus"></i> </span>
+			</div>
+		</div>
+	</div>
+	<?php
 
-		woocommerce_quantity_input(
-			array(
-				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-			)
-		);
+	do_action( 'woocommerce_after_add_to_cart_quantity' );
+	?>
 
-		do_action( 'woocommerce_after_add_to_cart_quantity' );
-		?>
-
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
-
+		
+	<div class="sp-single-form-wrp">
+		<h4 class="sp-single-form-title">Persoonlijk shirt</h4>
+		<div class="sp-d2-frm-fields clearfix">
+			<div class="sp-d2-frm-field">
+			  <label>Naam</label>
+			  <input type="text" name="email" placeholder="Naam">
+			</div>
+			<div class="sp-d2-frm-field">
+			  <label>Nummer</label>
+			  <input type="text" name="email" placeholder="Nummer">
+			</div>
+		</div>
+		<div class="sp-frm-submit clearfix">
+			<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt sp-frm-submit-bdr"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+		</div>
+	</div>
 		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
 	</form>
 
